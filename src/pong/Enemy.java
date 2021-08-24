035101a8e9110e1784ca08656834548abd367ee4
Vitor Eliel Bezerra Freire <vitoreliel2011@hotmail.com>
2021-08-24 18:14:52 -0300

    Add files via upload

---
+++ src/pong/Enemy.java
@@ -0,0 +1,33 @@
+package pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+@SuppressWarnings("unused")
+public class Enemy {
+	
+	public double x,y;
+	public int width,height;
+	
+	public Enemy(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.width = 40;
+		this.height = 5;
+	}
+	
+	public void tick() {
+		x+= (Game.ball.x - x - 6) * 0.2;
+		
+		
+		
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect((int)x, (int)y, width, height);
+		
+	}
+
+}
